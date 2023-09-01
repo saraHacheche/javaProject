acbccef830b1e50a4e7e14d8f6bde93ed4efc782
ghildas75 <ghilassamir@gmail.com>
2023-08-31 22:45:03 -0400

    first commit

---
+++ umlProject/src/umlProject/Child.java
@@ -0,0 +1,12 @@
+package umlProject;
+
+public class Child extends Parent {
+private String	adress;
+
+
+
+public Child() {
+	super();
+}
+	
+}
---
+++ umlProject/src/umlProject/Parent.java
@@ -0,0 +1,27 @@
+package umlProject;
+
+public class Parent {
+	private String name;
+	private String firstName;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+
+}
